ad8a57a9c46d9f726d34f90a2e6d3997c77a3556
Joel Midstjärna <joel.midstjarna@gmail.com>
2021-11-21 16:02:22 +0100

    Submenus are working but placed wrongly

--- jdmenubar.js
+++ jdmenubar.js
@@ -2,40 +2,37 @@ class MenuBar {
     constructor(menuBarElement, menuItems) {
         this.menuBarElement = menuBarElement;
         menuBarElement.classList.add("jdmenu-bar");
-        this.parseMenuItems(menuBarElement, menuItems, true);
+        this.parseMenuItems(menuBarElement, menuItems);
     }
 
     // Recursive function for generating menu item elements and
     // sub menu elements 
-    parseMenuItems(menuElement, menuItems, topLevel) {
+    parseMenuItems(menuElement, menuItems) {
         for (var i = 0 ; i < menuItems.length ; i++) {
             const menuItem = menuItems[i];
             const menuItemElement = document.createElement("div");
             menuItemElement.classList.add("jdmenu-item");
             menuItemElement.innerText = menuItem["text"];
-            menuItemElement.setAttribute("jdmenu_menu", menuElement);
             menuElement.appendChild(menuItemElement);
-            menuItem["_menuElement"] = menuElement; 
-            menuItem["_itemElement"] = menuItemElement;
 
             if ("subMenuItems" in menuItem) {
                 const subMenuElement = document.createElement("div");
                 subMenuElement.classList.add("jdmenu-submenu");
+                subMenuElement.jdmenu_parentItem = menuItemElement;
+                subMenuElement.jdmenu_parentMenu = menuElement;
                 menuElement.appendChild(subMenuElement);
-                menuItem["_subMenuElement"] = subMenuElement;
                 this.parseMenuItems(subMenuElement, menuItem["subMenuItems"], false);
                 const myself = this;
                 menuItemElement.onclick = function () {
-                    myself.toggleSubMenu(myself, menuItem, topLevel);
+                    myself.toggleSubMenu(myself, subMenuElement);
                 };
             }
         }
     }
 
-    // dropDown is sub menu shall appear down or to the right 
-    toggleSubMenu(myself, menuItem, dropDown) {
-        const menuItemElement = menuItem["_itemElement"];
-        const subMenuElement = menuItem["_subMenuElement"];
+    // myself is this object. Needed since the method is called as a 
+    // callback function.
+    toggleSubMenu(myself, subMenuElement) {
         if (subMenuElement.style.display == "block") {
             subMenuElement.style.display = "none";
         }
@@ -46,13 +43,20 @@ class MenuBar {
                 element.style.display = "none";
             }
             // But display the menu that we just opened and parents
-            subMenuElement.style.display = "block";
+            var menuItem = subMenuElement;
+            while(menuItem != myself.menuBarElement) {
+                menuItem.style.display = "block";
+                menuItem = menuItem.jdmenu_parentMenu;
+            }
 
+            const menuItemElement = subMenuElement.jdmenu_parentItem; 
             const pos = menuItemElement.getBoundingClientRect();
-            if (dropDown) {
+            if (subMenuElement.jdmenu_parentMenu == myself.menuBarElement) {
+                // Drop down
                 subMenuElement.style.left = pos.left;
                 subMenuElement.style.top = pos.bottom;
             } else {
+                // Show right
                 subMenuElement.style.left = pos.right;
                 subMenuElement.style.top = pos.top;
             }
