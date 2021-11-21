7adbad83132ff2d7b2a830470d340fbb4815fdf3
Joel Midstjärna <joel.midstjarna@gmail.com>
2021-11-21 16:58:39 +0100

    Now sub menues are closed correctly

--- jdmenubar.js
+++ jdmenubar.js
@@ -1,6 +1,7 @@
 class MenuBar {
     constructor(menuBarElement, menuItems) {
         this.menuBarElement = menuBarElement;
+        this.stateOpen = false;
         menuBarElement.classList.add("jdmenu-bar");
         this.parseMenuItems(menuBarElement, menuItems);
     }
@@ -8,11 +9,13 @@ class MenuBar {
     // Recursive function for generating menu item elements and
     // sub menu elements 
     parseMenuItems(menuElement, menuItems) {
+        const myself = this;
         for (var i = 0 ; i < menuItems.length ; i++) {
             const menuItem = menuItems[i];
             const menuItemElement = document.createElement("div");
             menuItemElement.classList.add("jdmenu-item");
             menuItemElement.innerText = menuItem["text"];
+            menuItemElement.jdmenu_menu = menuElement;
             menuElement.appendChild(menuItemElement);
 
             if ("subMenuItems" in menuItem) {
@@ -22,10 +25,21 @@ class MenuBar {
                 subMenuElement.jdmenu_parentMenu = menuElement;
                 menuElement.appendChild(subMenuElement);
                 this.parseMenuItems(subMenuElement, menuItem["subMenuItems"], false);
-                const myself = this;
                 menuItemElement.onclick = function () {
                     myself.toggleSubMenu(myself, subMenuElement);
                 };
+                menuItemElement.onmouseover = function () {
+                    if (myself.stateOpen) {
+                        myself.openSubMenu(myself, subMenuElement);
+                    }
+                };
+            } else {
+                if (menuElement != this.menuBarElement) {
+                    // To close other sub-menues
+                    menuItemElement.onmouseover = function () {
+                        myself.openSubMenu(myself, menuElement);
+                    };
+                }
             }
         }
     }
@@ -33,36 +47,53 @@ class MenuBar {
     // myself is this object. Needed since the method is called as a 
     // callback function.
     toggleSubMenu(myself, subMenuElement) {
+        const menuItemElement = subMenuElement.jdmenu_parentItem; 
         if (subMenuElement.style.display == "block") {
-            subMenuElement.style.display = "none";
+            this.closeSubMenu(myself, subMenuElement);
         }
         else {
-            // Hide all menus
-            const subMenuElements = myself.menuBarElement.getElementsByClassName("jdmenu-submenu");
-            for (var element of subMenuElements) {
-                element.style.display = "none";
-            }
-            // But display the menu that we just opened and parents
-            var menuItem = subMenuElement;
-            while(menuItem != myself.menuBarElement) {
-                menuItem.style.display = "block";
-                menuItem = menuItem.jdmenu_parentMenu;
-            }
-
-            const menuItemElement = subMenuElement.jdmenu_parentItem; 
-            const pos = menuItemElement.getBoundingClientRect();
-            console.log(pos);
-            if (subMenuElement.jdmenu_parentMenu == myself.menuBarElement) {
-                // Drop down
-                subMenuElement.style.left = pos.left;
-                subMenuElement.style.top = pos.bottom;
-            } else {
-                // Show right
-                subMenuElement.style.left = pos.right;
-                subMenuElement.style.top = pos.top;
-            }
+            this.openSubMenu(myself, subMenuElement);
         }
     }
 
+     // myself is this object. Needed since the method is called as a 
+    // callback function.
+    closeSubMenu(myself, subMenuElement) {
+        const menuItemElement = subMenuElement.jdmenu_parentItem; 
+        subMenuElement.style.display = "none";
+        if (menuItemElement.jdmenu_menu == myself.menuBarElement) {
+            // Closed top element. Menu is closed
+            myself.stateOpen = false;
+        }
+    }   
+
+    // myself is this object. Needed since the method is called as a 
+    // callback function.
+    openSubMenu(myself, subMenuElement) {
+        const menuItemElement = subMenuElement.jdmenu_parentItem; 
+        myself.stateOpen = true; // May be overridden below
+        // Hide all menus
+        const subMenuElements = myself.menuBarElement.getElementsByClassName("jdmenu-submenu");
+        for (var element of subMenuElements) {
+            element.style.display = "none";
+        }
+        // But display the menu that we just opened and parents
+        var menuItem = subMenuElement;
+        while(menuItem != myself.menuBarElement) {
+            menuItem.style.display = "block";
+            menuItem = menuItem.jdmenu_parentMenu;
+        }
+
+        const pos = menuItemElement.getBoundingClientRect();
+        if (subMenuElement.jdmenu_parentMenu == myself.menuBarElement) {
+            // Drop down
+            subMenuElement.style.left = pos.left;
+            subMenuElement.style.top = pos.bottom;
+        } else {
+            // Show right
+            subMenuElement.style.left = pos.right;
+            subMenuElement.style.top = pos.top;
+        }
+    }
     
 }
