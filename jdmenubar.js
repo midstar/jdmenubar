04c566ed6dd06dcc4238b5196f73319d291c51de
Joel Midstjärna <joel.midstjarna@gmail.com>
2021-11-21 15:24:01 +0100

    Multiple sub menus

--- jdmenubar.js
+++ jdmenubar.js
@@ -1,56 +1,61 @@
 class MenuBar {
     constructor(menuBarElement, menuItems) {
-        this.menuItems = menuItems;
+        this.menuBarElement = menuBarElement;
         menuBarElement.classList.add("jdmenu-bar");
+        this.parseMenuItems(menuBarElement, menuItems, true);
+    }
 
-        const callbackToggleSubMenu = this.toggleSubMenu;
-
-        menuItems.forEach(function(menuItem) {
+    // Recursive function for generating menu item elements and
+    // sub menu elements 
+    parseMenuItems(menuElement, menuItems, topLevel) {
+        for (var i = 0 ; i < menuItems.length ; i++) {
+            const menuItem = menuItems[i];
             const menuItemElement = document.createElement("div");
             menuItemElement.classList.add("jdmenu-item");
             menuItemElement.innerText = menuItem["text"];
-            menuBarElement.appendChild(menuItemElement);
-            menuItem["_menuElement"] = menuBarElement; 
+            menuItemElement.setAttribute("jdmenu_menu", menuElement);
+            menuElement.appendChild(menuItemElement);
+            menuItem["_menuElement"] = menuElement; 
             menuItem["_itemElement"] = menuItemElement;
 
             if ("subMenuItems" in menuItem) {
                 const subMenuElement = document.createElement("div");
                 subMenuElement.classList.add("jdmenu-submenu");
-                menuItem["subMenuItems"].forEach(function(subMenuItem) {
-                    const subMenuItemElement = document.createElement("div");
-                    subMenuItemElement.classList.add("jdmenu-item");
-                    subMenuItemElement.innerText = subMenuItem["text"];
-                    subMenuElement.appendChild(subMenuItemElement);
-                    subMenuItem["_menuElement"]=subMenuElement;
-                    subMenuItem["_itemElement"];
-                    subMenuItem["_parent"] = menuItem;
-                });
-                menuBarElement.appendChild(subMenuElement);
+                menuElement.appendChild(subMenuElement);
                 menuItem["_subMenuElement"] = subMenuElement;
+                this.parseMenuItems(subMenuElement, menuItem["subMenuItems"], false);
+                const myself = this;
                 menuItemElement.onclick = function () {
-                    callbackToggleSubMenu(menuItem);
+                    myself.toggleSubMenu(myself, menuItem, topLevel);
                 };
             }
-        });
+        }
     }
 
-    // Recursive
-    parseMenuItems(menuElement, menuItems) {
-
-    }
-
-    toggleSubMenu(menuItem) {
+    // dropDown is sub menu shall appear down or to the right 
+    toggleSubMenu(myself, menuItem, dropDown) {
         const menuItemElement = menuItem["_itemElement"];
         const subMenuElement = menuItem["_subMenuElement"];
         if (subMenuElement.style.display == "block") {
             subMenuElement.style.display = "none";
         }
         else {
-            // Put sub menu in the bottom left position of menu menuBarElement
-            const pos = menuItemElement.getBoundingClientRect();
-            subMenuElement.style.left = pos.left;
-            subMenuElement.style.top = pos.bottom;
+            // Hide all menus
+            const subMenuElements = myself.menuBarElement.getElementsByClassName("jdmenu-submenu");
+            for (var element of subMenuElements) {
+                element.style.display = "none";
+            }
+            // But display the menu that we just opened and parents
             subMenuElement.style.display = "block";
+
+            const pos = menuItemElement.getBoundingClientRect();
+            if (dropDown) {
+                subMenuElement.style.left = pos.left;
+                subMenuElement.style.top = pos.bottom;
+            } else {
+                subMenuElement.style.left = pos.right;
+                subMenuElement.style.top = pos.top;
+            }
         }
     }
 
